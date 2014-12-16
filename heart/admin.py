59d949265d16cf28bad3cec616774a04cf80802e
gowanpeter <peterstewart@telus.net>
2014-12-16 10:55:41 -0800

    simple_admin_works

--- heart/admin.py
+++ heart/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.db import models
+from .models import Piece
 
 # Register your models here.
+class PieceAdmin(admin.ModelAdmin):
+    pass
+    
+admin.site.register(Piece, PieceAdmin)
