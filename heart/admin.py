93fcc7edaef8c29efe8b0ffd4da888e1f34b79ae
gowanpeter <peterstewart@telus.net>
2014-12-17 10:47:55 -0800

    editing_admin.py

--- heart/admin.py
+++ heart/admin.py
@@ -1,9 +1,34 @@
+#heart
 from django.contrib import admin
 from django.db import models
-from .models import Piece
+from .models import Piece, ConditionChoice, Publication, Exhibition
 
 # Register your models here.
 class PieceAdmin(admin.ModelAdmin):
-    pass
+	pass
     
 admin.site.register(Piece, PieceAdmin)
+
+class ConditionChoiceAdmin(admin.ModelAdmin):
+	pass	
+
+	admin.site.register(ConditionChoice, ConditionChoiceAdmin)
+
+#one to many
+#one publication has many pieces, foreign key is publication_id and is in table 'Pieces'
+#publication = models.ForeignKey('publication')
+
+class PublicationAdmin(admin.ModelAdmin):
+	pass	
+	
+	admin.site.register(Publication, PublicationAdmin)
+
+
+#one to many
+#one exhibition has many pieces, foreign key is exibition_id and is in pieces
+#exhibition = models.ForeignKey('exhibition')
+class ExhibitionAdmin(admin.ModelAdmin):
+	pass	
+	
+	admin.site.register(Exhibition, ExhibitionAdmin)
+
--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,4 @@
+#places
 from django.contrib import admin
 
 # Register your models here.
