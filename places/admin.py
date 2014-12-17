7a372a5fe5b4a6ca693a3dfee8c346dc5ac07e44
gowanpeter <peterstewart@telus.net>
2014-12-17 10:54:48 -0800

    admins_all_done_first_time

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,19 @@
 #places
 from django.contrib import admin
 
-# Register your models here.
+class DocumentationAdmin(admin.ModelAdmin):
+
+	admin.site.register(Documentation, DocumentationAdmin)
+	
+class documentation_link_pieceAdmin(admin.ModelAdmin):
+
+	admin.site.register(documentation_link_piece, documentation_link_pieceAdmin)
+	
+#many to many
+class Set_CollectionAdmin(admin.ModelAdmin):
+
+	admin.site.register(Set_Collection, Set_CollectionAdmin)
+
+class SetCollection_link_pieceAdmin(admin.ModelAdmin):
+
+	admin.site.register(SetCollection_link_piece, SetCollection_link_pieceAdmin)
